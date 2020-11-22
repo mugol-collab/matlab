84daac1780c96362b867304a0d0975f3f54cb236
Juan <juan_jg@hotmail.com>
2020-11-22 13:58:07 -0300

    Adição comentarios Distribução Normal

--- MQI2104/aula01_ex05_randn.m
+++ MQI2104/aula01_ex05_randn.m
@@ -3,7 +3,7 @@
 % Para obtener distribuções normais
 % com outros parámetros [u, o]
 %
-% u + rand() + o
+% u + rand() * o     ==   o * rand() + u
 %
 % onde
 % u: media
@@ -14,6 +14,7 @@ clear all;
 close all;                      % media desvioPadrão 
                                 %    u    o   
 data3 = randn(1, 500) + 3;      %   [3,   1]
-data4 = randn(1, 500) * 3 + 1;  %   [1,   3]
+% data4 = randn(1, 500) * 3 + 1;  %   [1,   3]
+data4 = 3 * randn(1, 500) + 1;  %   [1,   3]
 subplot(2, 1, 1), plot(data3), axis([0 500 -10 10])
 subplot(2, 1, 2), plot(data4), axis([0 500 -10 10])
