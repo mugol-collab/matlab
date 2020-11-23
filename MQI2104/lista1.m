5007aa29738baccfaa4af8e96ec031d626e9041a
Juan <juan_jg@hotmail.com>
2020-11-23 03:00:23 -0300

    exercicios lista1

--- MQI2104/lista1.m
+++ MQI2104/lista1.m
@@ -1,31 +1,41 @@
+%% MQI 2104: Processamento e Análise de Sinais Digitais
+%% PRIMEIRA LISTA DE EXERCÍCIOS
+%% Professor: Carlos Hall
+%% Aluno: Juan Gómez
+
 clc;
 clear all;
 close all;
 
 % Tabela de dados distância vs tensão 
-tensaoSensor = table2array(readtable("tabelaLista1.xlsx", "Range", "B3:K35"));
+tensaoSensor = table2array(readtable("Lista1.xlsx", "Range", "B3:K35"));
+distancias = table2array(readtable("lista1.xlsx", "Range", "A3:A35"));
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 1. Tensão média correspondente a cada valor de distância
 
 tensaoMedia = zeros(1,33);
 
 for i = 1:33
-   d = tensaoSensor(i,:);
-   tensaoMedia(i) = mean(d)
+   tensoes = tensaoSensor(i,:);
+   tensaoMedia(i) = mean(tensoes);
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 2. Interpolação linear
 
-x = table2array(readtable("tabelaLista1.xlsx", "Range", "A3:A35"));
+x = distancias';
 y = tensaoMedia;
-plot(x', y, '.', 'markersize', 20)
-xlim([0 20]), ylim([0 2.5])
+
+plot(x, y, '.', 'markersize', 20)
+xlim([0 20]), ylim([-0.5 2.5])
 xlabel('Distância (cm)'), ylabel('Tensâo (V)')
-xi = [0.150 0.673 1.520 1.820 2.205];
-yi = interp1(x, y, xi);
+title('Interpolação linear')
+grid on;
+
+yi = [0.150 0.673 1.520 1.820 2.205];
 hold on
+plot(xlim, [yi; yi], 'r-.')
+xi = interp1(x, y, yi);
 plot(xi, yi, 'm.', 'markersize', 20)
-% plot(x, y, 'g')
+plot(x, y, 'b')
+legend('Média tensões calibração (V)', 'Medições em campo')
 
