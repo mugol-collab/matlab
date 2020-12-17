856c5e7cc82c03915b1e5449a5685252adb8afbf
Juan <juan_jg@hotmail.com>
2020-12-17 08:25:39 -0300

    atualizacao lista 1 e lista 2 sinais

--- MQI2104/lista2.m
+++ MQI2104/lista2.m
@@ -8,16 +8,38 @@ clc;
 clear all;
 close all;
 
-n = -5:4;
+n0 = -5:4;
 x = [0 -1 -0.5 0.5 1 1 1 1 0.5 0];
 
-subplot(2, 2, [1 2])
-stem(n, x)
-xlim([-6 5]), ylim([-2 2])
-title("Sequência principal")
-text(-5, 1.8,'[ 0 -1 -0.5 0.5 1 1 1 1 0.5 0]','FontSize',10,'Color','blue')
-text(-5, 1.6,'[-5 -4  -3   -2  -1 0 1 2  3  4]','FontSize',10,'Color','blue')
+%%% x[n]
+subplot(4, 2, [1 2])
+stem(n0, x)
+xlim([-6 10]), ylim([-2 2])
+title("x[n]")
+% text(-5, 1.8,'[ 0 -1 -0.5 0.5 1 1 1 1 0.5 0]','FontSize',10,'Color','blue')
+% text(-5, 1.4,'[-5 -4  -3   -2  -1 0 1 2  3  4]','FontSize',10,'Color','blue')
 grid on;
 
-subplot(2, 2, 3)
-stem(n, x - 4)
+%% (a) x[n-4]
+subplot(4, 2, 3)
+n1 = -1:8;
+stem(n1, x(n1 - min(n1) + 1))
+xlim([-6 10]), ylim([-2 2])
+title("x[n-4]")
+grid on;
+
+%% (b) x[3-n]
+subplot(4, 2, 4)
+
+%% (c) x[3n]
+subplot(4, 2, 5)
+
+%% (d) x[3n+1]
+subplot(4, 2, 6)
+
+%% (e) x[n] u[3-n]
+subplot(4, 2, 7)
+
+%% (f) x[n-2]  o[n–2] 
+subplot(4, 2, 8)
+
